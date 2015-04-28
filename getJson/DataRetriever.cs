24c1935061a40a0dd9f7dc954ca1bd87e71ac244
patterncoder <chris@oldtowndining.com>
2015-04-28 14:02:01 -0700

    first commit

---
+++ getJson/DataRetriever.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace getJson
+{
+    public class DataRetriever
+    {
+        private string _conn;
+        public DataRetriever()
+        {
+            _conn = ConfigurationManager.ConnectionStrings["SiteSqlServer"].ConnectionString;
+        }
+
+        public string getJson(string now)
+        {
+
+            string result;
+            string sql = "otd_sp_entertainmentSchedule";
+            
+            using(SqlConnection myConnection = new SqlConnection(_conn))
+            {
+                using(SqlCommand myCommand = new SqlCommand(sql, myConnection))
+                {
+                    myCommand.CommandType = CommandType.StoredProcedure;
+                    myCommand.Parameters.AddWithValue("@date", now);
+                    myConnection.Open();
+                    
+                    DataTable dt = new DataTable();
+                    dt.Load(myCommand.ExecuteReader());
+                    result = JsonConvert.SerializeObject(dt);
+                }
+            }
+
+            return result;
+
+            //JsonConvert.SerializeObject();
+        }
+        
+        
+
+    }
+}
---
+++ testHarness/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using getJson;
+
+namespace testHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hello from the console \r\n");
+            var jsonGetter = new getJson.DataRetriever();
+            Console.Write(jsonGetter.getJson("20150425"));
+            Console.Read();
+        }
+    }
+}
